617f0b984af0a484c03ece63f28407cf89869994
Manu puranic <68289159+manupuranic@users.noreply.github.com>
2022-10-18 14:47:37 +0530

    set debug=False

--- todo/settings.py
+++ todo/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-506=h%n+f1i4%7zixgf#pp9e3%nsbeeatw9$3k7i&080o7*ms6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
