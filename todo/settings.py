5da706bdc1bf2dd0e870a97c012e4acac393cf6a
Manu puranic <68289159+manupuranic@users.noreply.github.com>
2022-10-18 13:34:36 +0530

    settings changed

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-506=h%n+f1i4%7zixgf#pp9e3%nsbeeatw9$3k7i&080o7*ms6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
